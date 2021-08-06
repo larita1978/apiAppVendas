d6eb080bc532fcb849d42eb7621b1b72b108ca6f
larita1978 <larissainesdemorais@hotmail.com>
2021-08-05 23:20:55 -0300

    first commit

---
+++ src/main/java/com/appVendas/apiAppVendas/ApiAppVendasApplication.java
@@ -0,0 +1,13 @@
+package com.appVendas.apiAppVendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAppVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAppVendasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/appVendas/apiAppVendas/ApiAppVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appVendas.apiAppVendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAppVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
